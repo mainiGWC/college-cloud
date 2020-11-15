991c22d16d1bd9bf4e6526e22a3f5fdedf52e70e
Glitch (hello-webpage) <none>
2020-11-15 00:49:15 +0000

    🛋🍿 Checkpoint ./testing.html:1434827/889 ./style.css:1434816/1409:1434827/476 ./faqs.html:1434816/581 ./script.js:1434816/370

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ for (i = 0; i < fol.length; i++) {
     }
   });
 }
+
+var fol = document.getElementsByClassName("foldingFAQ");
+var i;
+
+for (i = 0; i < fol.length; i++) {
+  fol[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var board = this.nextElementSibling;
+    if (board.style.display === "block") {
+      board.style.display = "none";
+    } else {
+      board.style.display = "block";
+    }
+  });
+}
