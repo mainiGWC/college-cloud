c27ecce350bc825f782a2ef4dd28b8ce0b892c5b
Glitch (hello-webpage) <none>
2020-11-15 06:11:55 +0000

    👆🎨 Checkpoint ./style.css:1434816/31:25057247/152 ./activities.html:25057247/259 ./index.html:1434816/129:1434827/68 ./script.js:1434827/1670

--- script.js
+++ script.js
@@ -30,39 +30,20 @@ for (i = 0; i < fol.length; i++) {
   });
 }
 
-// /*Animation for slideshow in testing page*/
-// var slideIndex = 0;
-// showSlides();
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";  
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 4000); // Change image every 4 seconds
-// }
-/*Animation for home slideshow*/
-// var index = 0;
-// automaticCarousel();
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-// function automaticCarousel() {
-//   var i;
-//   var x = document.getElementsByClassName("homeImages");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";  
-//   }
-//   index++;
-//   if (index > x.length) {index = 1}    
-//   x[index-1].style.display = "block";  
-//   setTimeout(automaticCarousel, 4000); // Change image every 4 seconds
-// }
-// 
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
