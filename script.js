6ba9bd6d00797c0b70153eedc1f00525392766ee
Glitch (hello-webpage) <none>
2020-11-15 06:23:20 +0000

    👆👢 Checkpoint ./style.css:1434816/106:25057247/29 ./activities.html:25057247/1733 ./.glitch-assets:1434816/394 ./index.html:1434816/432:25057247/384 ./script.js:1434827/58

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ for (i = 0; i < fol.length; i++) {
 }
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickyScroll()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -40,7 +40,7 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function stickyScroll() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
