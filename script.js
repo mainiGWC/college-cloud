d99e992a2b3a8e3e17c5b2cf0d8d6a2e4f141de6
Glitch (hello-webpage) <none>
2020-11-15 02:10:43 +0000

    👛💡 Checkpoint ./style.css:1434816/457:1434827/196 ./index.html:1434827/2490 ./script.js:1434827/1570 ./testing.html:1434816/344

--- script.js
+++ script.js
@@ -30,23 +30,39 @@ for (i = 0; i < fol.length; i++) {
   });
 }
 
-/*Animation for slideshow in testing page*/
-var slideIndex = 0;
-showSlides();
+// /*Animation for slideshow in testing page*/
+// var slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 4000); // Change image every 4 seconds
+// }
+/*Animation for home slideshow*/
+// var index = 0;
+// automaticCarousel();
+
+// function automaticCarousel() {
+//   var i;
+//   var x = document.getElementsByClassName("homeImages");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   index++;
+//   if (index > x.length) {index = 1}    
+//   x[index-1].style.display = "block";  
+//   setTimeout(automaticCarousel, 4000); // Change image every 4 seconds
+// }
+// 
